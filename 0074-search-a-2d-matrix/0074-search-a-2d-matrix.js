a09a5039c3a4041347f234e91c5275a678619303
임승현 <126482821+96limshyun@users.noreply.github.com>
2025-03-25 19:22:53 +0900

    Time: 0 ms (100%), Space: 53.6 MB (39.47%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -6,37 +6,40 @@
 var searchMatrix = function(matrix, target) {
     const ROW = matrix.length;
     const COL = matrix[0].length;
-    
+
     let top = 0;
     let bot = ROW - 1;
-    let row = 0;
-    
-    while(top <= bot){
+
+    while(top <= bot) {
         const mid = Math.floor((top + bot) / 2)
         if(target > matrix[mid][COL - 1]) {
-           top = mid + 1
+            top = mid + 1
         } else if (target < matrix[mid][0]) {
-           bot = mid - 1;
+            bot = mid - 1
         } else {
-            row = mid;
             break;
         }
     }
-    
-    if (top > bot) return false;
+
+    if(!(top <= bot)) {
+        return false;
+    }
+
+    const row = Math.floor((top + bot) / 2)
     
     let left = 0;
     let right = COL - 1;
-    
-    while(left <= right){
+
+    while(left <= right) {
         const mid = Math.floor((left + right) / 2)
         if(target > matrix[row][mid]) {
-           left = mid + 1;
+            left = mid + 1
         } else if (target < matrix[row][mid]) {
-           right = mid - 1
+            right = mid - 1
         } else {
-          return true                
+            return true
         }
     }
+
     return false
 };
