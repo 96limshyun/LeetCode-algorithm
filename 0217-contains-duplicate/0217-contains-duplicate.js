2a3e65c7880bcbd98a4a7cf164c2f24774252f8a
임승현 <126482821+96limshyun@users.noreply.github.com>
2025-07-19 14:22:39 +0900

    Time: 11 ms (95.02%), Space: 73.2 MB (78.61%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,13 +3,15 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    nums.sort((a, b) => a - b);
-    
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i] === nums[i - 1]) {
-            return true;
+    const set = new Set()
+
+    for(let i = 0; i < nums.length; i++) {
+        const num = nums[i]
+        const hasNum = set.has(num)
+        if(hasNum) {
+            return true
         }
+        set.add(num)
     }
-    
-    return false;    
+    return false
 };
